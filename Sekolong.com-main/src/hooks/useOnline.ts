885c77996643c14cc29a0d863ac2c0a5863b5e30
morenahabotswe <morenahabotsoe@gmail.com>
2026-09-25 16:04:11 +0200

    Delete Sekolong.com-main directory

--- Sekolong.com-main/src/hooks/useOnline.ts
+++
@@ -1,2 +0,0 @@
-import {useEffect,useState} from 'react';
-export function useOnline(){const [online,setOnline]=useState(navigator.onLine); useEffect(()=>{const on=()=>setOnline(true),off=()=>setOnline(false); window.addEventListener('online',on);window.addEventListener('offline',off);return()=>{window.removeEventListener('online',on);window.removeEventListener('offline',off)}},[]);return online}
